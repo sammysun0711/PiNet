e1883f3aa02db7952346ff55493111d63e58b072
Xiake Sun <sammysun0711@gmail.com>
2018-01-09 00:38:56 +0100

    first build to test function

---
+++ build/CMakeFiles/PiNet.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PiNet.dir/pinet.cxx.o"
+  "PiNet.pdb"
+  "PiNet"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PiNet.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
