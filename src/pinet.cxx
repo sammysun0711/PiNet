6d486a2f7382decbc370d16dfc4ca06679c43e36
Xiake Sun <sammysun0711@gmail.com>
2018-01-09 21:41:43 +0100

    add function initSet, delSet,initVar, delVar, resetVal

--- src/pinet.cxx
+++ src/pinet.cxx
@@ -8,7 +8,7 @@
 #include <string>
 
 
-using namespace std;
+//using namespace std;
 using namespace boost::network;
 using namespace boost::network::http;
 using namespace rapidjson; 
@@ -23,6 +23,13 @@ const std::string usr = "Xiake";
 const std::string pwd = "123";    
 
 //helper function for check if value is number
+/**
+ * White space will make it return false.
+ * NAN and INF will make it return false (to be exact, any character except valid exponent will make it return false). If you want to allow nan and inf, delete the || std::isalpha(s[0]) part.
+ * scientific form is allowed i.e 1e+12 will return true.
+ * Double/float or integer will return true. 
+ * /
+**/
 template<typename T>
 bool isNumber(T x){
    std::string s;
@@ -34,6 +41,7 @@ bool isNumber(T x){
    strtod(s.c_str(), &p) ;
    return (*p == 0) ;
 }
+
 
 //helper function for concrete [] part from httpResponse        
 std::string tail(std::string const& source, size_t const length) {
@@ -49,44 +57,42 @@ void parse(std::string reply, std::string setname, std::string varname, std::str
         int count;
         count = reply.length()-reply.find(TRIGGER_RESPONSE_MYSQL)-TRIGGER_RESPONSE_MYSQL.length();
         strJson = tail(reply, count);
-        cout<< endl<< endl << strJson <<endl;
+        //cout<< endl<< endl << strJson <<endl;
         // if there is json in [], parse json into doc, otherweise return no json in reply from server
         if (strJson.length()>2){
-        strJson.erase(0,1);
-        strJson.erase(strJson.size()-1);
-        cout<< endl << endl << strJson <<endl;
+            strJson.erase(0,1);
+            strJson.erase(strJson.size()-1);
+            std::cout<< std::endl << std::endl << strJson << std::endl;
     
-        Document doc;
-        try{
-            if(doc.Parse<0>(strJson.c_str()).HasParseError())
-            cout << "ERROR" << endl;
-            else
-            cout << "OK" << endl;
-
-            doc.Parse(strJson.c_str());
-            std::string setname_ = doc["setname"].GetString();
-            std::string varname_ = doc["varname"].GetString();
-            std::string datatype_ = doc["datatype"].GetString();
+            Document doc;
+            try{
+                if(!doc.Parse<0>(strJson.c_str()).HasParseError()){
+                    doc.Parse(strJson.c_str());
+                    std::string setname_ = doc["setname"].GetString();
+                    std::string varname_ = doc["varname"].GetString();
+                    std::string datatype_ = doc["datatype"].GetString();
         
-            if (setname_ == setname && varname_==varname && datatype_ == datatype){
-            std::string value_ = doc["value"].GetString();
-            cout << "setname = " << setname_ << endl;
-            cout << "varname = " << varname_ << endl;
-            cout << "datatype = " << datatype_ << endl;
-            cout << "value = " << value_ << endl;
-            }
-            else{
-                cout << "there is no valid content in reply" << endl;
-            }
-        } 
+                    if (setname_ == setname && varname_==varname && datatype_ == datatype){
+                        std::string value_ = doc["value"].GetString();
+                        std::cout << std::endl <<  "parse result:" << std::endl;
+                        std::cout << "setname = " << setname_ << std::endl;
+                        std::cout << "varname = " << varname_ << std::endl;
+                        std::cout << "datatype = " << datatype_ << std::endl;
+                        std::cout << "value = " << value_ << std::endl;
+                    }
+                    else{
+                        std::cerr << std::endl <<  "parse result : there is no valid content in reply" << std::endl;
+                    }
+                }
+            }    
 
-        catch (std::exception &e) {
+            catch (std::exception &e) {
             std::cerr << e.what() << std::endl;
-        }
+            }
         }
 
         else if(strJson == "[]"){
-            std::cerr << "there is no json in reply from server" << endl;   
+            std::cerr << std::endl <<"parse result : there is no Json in reply from server" << std::endl;   
         }
     }
     else if(reply.find(TRIGGER_ERRORS_MYSQL)!=std::string::npos){
@@ -95,20 +101,21 @@ void parse(std::string reply, std::string setname, std::string varname, std::str
         strJson = tail(reply, count);
         strJson.erase(0,1);
         strJson.erase(strJson.size()-1);
-        cout << strJson << endl;
+        std::cout << strJson << std::endl;
+
         Document doc;
         if(!doc.Parse<0>(strJson.c_str()).HasParseError()){
-        doc.Parse(strJson.c_str());
-        std::stringstream failure;
-        failure << doc["errorCode"].GetString() << " - " << doc["errorMessage"].GetString();
-        cout << failure.str() << endl;
+            doc.Parse(strJson.c_str());
+            std::stringstream failure;
+            failure << doc["errorCode"].GetString() << " - " << doc["errorMessage"].GetString();
+            std::cout << failure.str() << std::endl;
         }
     }
     else if (reply.find(TRIGGER_ERRORS_PHP)!=std::string::npos){
         int count;
         count = reply.length()-reply.find(TRIGGER_ERRORS_PHP)-TRIGGER_ERRORS_PHP.length();
         std::string failure = tail(reply, count);
-        cout << endl << failure << endl;
+        std::cout << std::endl << failure << std::endl;
     }
 }
 
@@ -122,27 +129,27 @@ std::string sendRequest(std::string prozedur, std::list<std::string> parameter){
           << "&prozedur=" << uri::encoded(prozedur);
 
     for (std::list<std::string>::const_iterator iterator = parameter.begin(), end = parameter.end(); iterator != end; ++iterator) {
-    std::string value = *iterator;
-    std::string vtyp = "string";
-    bool isNumber_ = false;
+        std::string value = *iterator;
+        std::string vtyp = "string";
+        bool isNumber_ = false;
 
-    isNumber_ = isNumber(value);
-    if (value == "0")
-        vtyp = "zero";
-    else if (value.empty() || value == "")
-        vtyp = "emptyString";
-    else if (isNumber_)
-        vtyp = "zahl";
+        isNumber_ = isNumber(value);
+        if (value == "0")
+            vtyp = "zero";
+        else if (value.empty() || value == "")
+            vtyp = "emptyString";
+        else if (isNumber_)
+            vtyp = "zahl";
     
-    std::string tmpVtyp = "&vtyp" + std::to_string(count) + "=";
-    query << tmpVtyp << uri::encoded(vtyp);
+        std::string tmpVtyp = "&vtyp" + std::to_string(count) + "=";
+        query << tmpVtyp << uri::encoded(vtyp);
 
-    std::string tmpVar = "&variable" + std::to_string(count) + "=";
-    query << tmpVar << uri::encoded(value);
+        std::string tmpVar = "&variable" + std::to_string(count) + "=";
+        query << tmpVar << uri::encoded(value);
          
-    count++;
+        count++;
     }
-    cout << query.str() << endl;
+    //cout << query.str() << endl;
     
     // HTTP request object
     //client::request httpRequest("http://posttestserver.com/post.php?dump");
@@ -153,9 +160,9 @@ std::string sendRequest(std::string prozedur, std::list<std::string> parameter){
     client httpClient;
     client::response httpResponse = httpClient.post(httpRequest, query.str());
     std::string httpResponseBody = body(httpResponse);
-    cout << endl << endl << "RESPONSE: " << httpResponseBody;
-    return httpResponseBody;
+    std::cout << std::endl << std::endl << "RESPONSE: " << httpResponseBody << std::endl;
     
+    return httpResponseBody;
 }
 
 // helper function for getting value of variable from databank
@@ -167,13 +174,14 @@ std::string getVal (std::string setname, std::string varname, std::string dataty
     parameter.push_back(setname);
     parameter.push_back(varname);
     parameter.push_back(datatype);
+
     return sendRequest("p_getVal", parameter);    
 } 
 
 
 // helper function for saving value of variable in databank 
 template <typename T>
-std::string saveVal (std::string setname, std::string varname, T value, std::string datatype, int row, int col){
+std::string saveVal (std::string setname, std::string varname, T value, std::string datatype, int row = 0 , int col = 0){
     
     std::list<std::string> parameter;
     
@@ -196,6 +204,57 @@ std::string saveVal (std::string setname, std::string varname, T value, std::str
     return sendRequest("p_saveValItem", parameter);    
 }
 
+std::string initSet( std::string setname){
+    std::list<std::string> parameter;
+    parameter.push_back(usr);
+    parameter.push_back(pwd);
+    parameter.push_back(setname);
+    return sendRequest("p_initSet", parameter);
+}
+
+std::string delSet( std::string setname){
+    std::list<std::string> parameter;
+    parameter.push_back(usr);
+    parameter.push_back(pwd);
+    parameter.push_back(setname);
+    
+    return sendRequest("p_delSet", parameter);
+}
+
+std::string initVar( std::string setname, int row, int col, std::string datatype, std::string varname, std::string unit){
+    std::list<std::string> parameter;
+    parameter.push_back(usr);
+    parameter.push_back(pwd);
+    parameter.push_back(setname);
+    parameter.push_back(std::to_string(row));
+    parameter.push_back(std::to_string(col));
+    parameter.push_back(datatype);
+    parameter.push_back(varname);
+    parameter.push_back(unit);
+    
+    return sendRequest("p_initVar", parameter);
+}
+
+std::string resetVal(std::string setname, std::string varname){
+    std::list<std::string> parameter;
+    parameter.push_back(usr);
+    parameter.push_back(pwd);
+    parameter.push_back(setname);
+    parameter.push_back(varname);
+
+    return sendRequest("p_resetVal", parameter);
+}
+
+std::string delVar(std::string setname, std::string varname){
+    std::list<std::string> parameter;
+    parameter.push_back(usr);
+    parameter.push_back(pwd);
+    parameter.push_back(setname);
+    parameter.push_back(varname);
+
+    return sendRequest("p_delVar", parameter);
+}
+
 int main()
 {   
     const std::string setname = "TestsetXiake";
@@ -215,8 +274,14 @@ int main()
     std::string reply_getVal = getVal(setname, varname, datatype);
     parse(reply_getVal, setname, varname, datatype);
 
-    std::string reply_saveVal = saveVal(setname, varname, value, datatype , 0, 0);
+    std::string reply_saveVal = saveVal(setname, varname, value, datatype);
     parse(reply_saveVal, setname, varname, datatype);
+
+    //std::string reply_initSet = initSet("initSet");
+    //std::string reply_delSet = delSet("initSet");
+    //std::string reply_initVar = initVar("initSet", 1, 1, datatype, varname, "");
+    //std::string reply_resetVal = resetVal("initSet", varname);
+    //std::string reply_delVar = delVar("initSet", varname);
     return 0;
 }
 
