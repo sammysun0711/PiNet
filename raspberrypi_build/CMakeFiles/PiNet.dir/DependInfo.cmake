57318f4e4d8fc9808029bf18bad56f3060630835
Xiake Sun <sammysun0711@gmail.com>
2018-01-14 20:47:02 +0100

    cross-compile for raspberry pi by using toolchain of raspberrypi

---
+++ raspberrypi_build/CMakeFiles/PiNet.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/sammysun/PiNet/src/pinet.cxx" "/home/sammysun/PiNet/raspberrypi_build/CMakeFiles/PiNet.dir/pinet.cxx.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/sammysun/PiNet/src/{$HOME/raspberrypi/tools/arm-bcm2708/arm-rpi-4.9.3-linux-gnueabihf/arm-linux-gnueabihf/sysroot/opt/vc/include}"
+  "/home/sammysun/PiNet/src/{$HOME/raspberrypi/tools/arm-bcm2708/arm-rpi-4.9.3-linux-gnueabihf/arm-linux-gnueabihf/sysroot/opt/vc/include/interface/vcos"
+  "/home/sammysun/PiNet/src/}"
+  "/home/sammysun/PiNet/src/{$HOME/raspberrypi/tools/arm-bcm2708/arm-rpi-4.9.3-linux-gnueabihf/arm-linux-gnueabihf/sysroot/opt/vc/include/interface/vcos/pthreads"
+  "/home/sammysun/PiNet/src/{$HOME/raspberrypi/tools/arm-bcm2708/arm-rpi-4.9.3-linux-gnueabihf/arm-linux-gnueabihf/sysroot/opt/vc/include/interface/vmcs_host/linux"
+  "."
+  "/home/sammysun/raspberrypi/tools/arm-bcm2708/arm-rpi-4.9.3-linux-gnueabihf/arm-linux-gnueabihf/sysroot/usr/lib/cmake/cppnetlib/../../../include"
+  "/home/sammysun/raspberrypi/tools/arm-bcm2708/arm-rpi-4.9.3-linux-gnueabihf/arm-linux-gnueabihf/sysroot/usr/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
